183382269b94b80e2dae8f5d20ffb77db96d8bc2
Felipe Martins dos Santos <felipemartinsss@gmail.com>
2025-06-07 09:41:03 -0300

    add fastapi endpoints to access the url shortener logic

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
-import hashlib
+from fastapi import FastAPI
+import uvicorn
 from url_shortener import URLShortener
+from pydantic import BaseModel
+app = FastAPI()
 
-def receive_input_url():
-    return input("Digite uma url: ")
+class URL(BaseModel):
+    value: str
 
-# TODO: Can be turned to an API
+@app.post("/api/v1/shorten")
+async def shorten(url: URL):
+    original_url = url.value
+    shortened_url = url_shortener.shorten(original_url)
+    return {
+        "shortened_url": shortened_url
+    }
+
+@app.post("/api/v1/recover")
+async def recover(url: URL):
+    shortened_url = url.value
+    original_url = url_shortener.recover(shortened_url)
+    return {
+        "original_url": original_url
+    }
+
 if __name__ == '__main__':
     url_shortener = URLShortener()
-    option = input("Digite 1 para obter uma url encurtada e 2 para recuperar uma url original: ")
-    if option == '1':
-        original_url = receive_input_url()
-        shortened_url = url_shortener.shorten(original_url)
-        url_shortener.print_urls(original_url, shortened_url)
-    elif option == '2':
-        shortened_url = input("Digite uma url encurtada: ")
-        original_url = url_shortener.recover(shortened_url)
-        url_shortener.print_urls(original_url, shortened_url)
-    else:
-        print("Opção inválida")
+    uvicorn.run(app, host = "127.0.0.1", port = 8000)
